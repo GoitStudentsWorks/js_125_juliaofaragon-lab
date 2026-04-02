ff00fc071c724bdf46e66c29bac70e09b1fe99b7
juliaofaragon <juliaofaragon@gmail.com>
2026-04-02 22:43:08 +0200

    fix: add deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import injectHTML from 'vite-plugin-html-inject';
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === 'serve' ? '/' : '/project_group2/',
   plugins: [injectHTML()],
   server: {
     port: 5173,
     open: false,
   },
-});
+}));
