672b666ded56605f8b7e9b096c19a5b0b4d38bf7
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2026-04-10 12:03:56 +0000

    Deploying to gh-pages from @ GoitStudentsWorks/js_125_juliaofaragon-lab@e131562104946dfca2cf20cd371d04196dd778a2 🚀

---
+++ furniture-modal-preview.js
@@ -0,0 +1,2 @@
+import{i as t}from"./assets/furniture-modal-wFGZ5y_K.js";import"./assets/vendor-DMIztpeO.js";function e(){t({forcePreview:!0})}document.readyState==="loading"?document.addEventListener("DOMContentLoaded",e):e();
+//# sourceMappingURL=furniture-modal-preview.js.map
